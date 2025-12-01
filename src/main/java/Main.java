e5254dc5143736a20a58c0dae352a05216cc3970
Maxim Ashewsky <ashewsky.maxim@yandex.ru>
2025-12-01 14:16:49 +0300

    Creating a calculator

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " +
-                Math.random());
+        System.out.println("Введите первое число:");
+        int number = new Scanner(System.in) .nextInt();
+        System.out.println("Введите второе число:");
+        int number1 = new Scanner(System.in) .nextInt();
+        double d = (double) number / number1;
+        System.out.println("Частное:" + d);
+        int s = (int) number + number1;
+        System.out.println("Сумма:" + s);
+        int r = (int) number - number1;
+        System.out.println("Разность:" + r);
+        int p = (int) number * number1;
+        System.out.println("Произведение:" + p);
+
     }
 }
