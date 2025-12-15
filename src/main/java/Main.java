f2ee9596dd4e27456267c198391107527bf5eac2
Maxim Ashewsky <ashewsky.maxim@yandex.ru>
2025-12-15 13:25:36 +0300

    Creating a cycle

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,35 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " +
-                Math.random());
+        int n = 0;
+        for (; ; ) {
+            System.out.println("Укажите путь до файла");
+            String path = new Scanner(System.in).nextLine();
+
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+
+            if (!file.exists()) {
+                System.out.println("Путь указан неверно");
+                continue;
+            }
+            if (isDirectory) {
+                System.out.println("Это путь до папки");
+                continue;
+            }
+            if (file.exists() && file.isFile()) {
+                n++;
+                System.out.println("Путь указан верно. Этот файл номер " + n);
+            }
+
+        }
     }
 }
+
+
+
+
+
