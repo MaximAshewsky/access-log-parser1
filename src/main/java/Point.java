39551fe7bbf5c695fa9180edbbde357b7799f921
Maxim Ashewsky <ashewsky.maxim@yandex.ru>
2026-01-20 15:28:30 +0300

    homework 8.1.1.1

--- src/main/java/Point.java
+++ src/main/java/Point.java
@@ -1,27 +1,39 @@
 import java.util.Objects;
 
 class Point {
-    int x;
-    int y;
+    private final int x;
+    private final int y;
 
     public Point(int x, int y) {
         this.x = x;
         this.y = y;
     }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
 }
     class Point3D extends Point{
-        int z;
+        private final int z;
 
         public Point3D(int x, int y, int z) {
             super(x, y);
             this.z = z;
         }
 
+        public int getZ() {
+            return z;
+        }
+
         @Override
         public String toString() {
             return "Point3D{" +
-                    "x=" + x +
-                    ", y=" + y +
+                    "x=" + getX() +
+                    ", y=" + getY() +
                     ", z=" + z +
                     '}';
         }
