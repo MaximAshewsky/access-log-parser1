a995c3259d22b6c1c37a9a0a68274020ee3eb47f
Maxim Ashewsky <ashewsky.maxim@yandex.ru>
2026-01-26 15:25:22 +0300

    homework.package.8.4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,15 @@
+
+import ru.courses.geometry.*;
+import java.awt.Point;
 public class Main {
     public static void main(String[] args) {
         Point p1 = new Point(1, 3);
-        Point p2 = new Point(1, 3);//В задании просят вывести оба сравнения false, хотя у нас три точки и сравнения должно быть три. Если прописать  Point p2 = p1, то в первом сравнении выведится true
+        Point p2 = new Point(1, 3);
         Point p3 = new Point(5, 8);
         System.out.println(p1);
         System.out.println(p2);
         System.out.println(p3);
-        System.out.println(p1 == p2);
-        System.out.println(p1 == p3);
-        System.out.println(p2 == p3);
+
     }
 }
 
--- src/main/java/ru/courses/geometry/Point.java
+++ src/main/java/ru/courses/geometry/Point.java
@@ -1,12 +1,20 @@
-import java.util.Objects;
+package ru.courses.geometry;
 
-class Point {
-    int x;
-    int y;
+public class Point {
+    final int x;
+    final int y;
 
     public Point(int x, int y) {
         this.x = x;
         this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
     }
 
     @Override
