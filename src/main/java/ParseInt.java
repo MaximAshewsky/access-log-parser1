0e136e65dec0032e99d6194e4567c15630a4ea1d
Maxim Ashewsky <ashewsky.maxim@yandex.ru>
2026-01-26 15:09:50 +0300

    homework.package.8.3.1

--- src/main/java/ParseInt.java
+++
@@ -1,9 +0,0 @@
-//public class ParseInt {
-//    int x = Integer.parseInt(args[0]);
-//    int y = Integer.parseInt(args[1]);
-//
-//    public ParseInt(int x, int y) {
-//        this.x = x;
-//        this.y = y;
-//    }
-//}
--- src/main/java/Pow.java
+++
@@ -1,7 +0,0 @@
-//public class Pow {
-//    double result = Math.pow(x, y);
-//
-//    public Pow(double result) {
-//        this.result = result;
-//    }
-//}
